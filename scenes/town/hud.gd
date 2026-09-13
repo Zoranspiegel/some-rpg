6a280691746fd2d0fad64a87bc7c0e46a0b8e142
Zoranspiegel <zoranbow@gmail.com>
2026-09-12 19:35:57 -0500

    Main Buttons

---
+++ scenes/town/hud.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+class_name HUD
+
+
+
+func _on_equipment_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_inventory_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_stats_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_skills_button_pressed() -> void:
+	pass # Replace with function body.
